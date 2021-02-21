f1235106b321a360df042118ed349915891ceb70
Ilya Ryabikin <ilyaryabikin0708@gmail.com>
2021-02-22 02:42:32 +0300

    Add lastOnlineChange property and lombok constructor annotations

--- src/main/java/ru/protei/test/domains/User.java
+++ src/main/java/ru/protei/test/domains/User.java
@@ -4,6 +4,7 @@ import static javax.persistence.GenerationType.IDENTITY;
 import static ru.protei.test.domains.Status.OFFLINE;
 
 import java.io.Serializable;
+import java.time.Instant;
 import javax.persistence.Column;
 import javax.persistence.Convert;
 import javax.persistence.Entity;
@@ -11,11 +12,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import org.hibernate.annotations.NaturalId;
 import org.springframework.data.domain.Persistable;
 import ru.protei.test.domains.constraints.PhoneNumber;
@@ -23,15 +28,19 @@ import ru.protei.test.domains.converters.StatusConverter;
 
 @Entity(name = "users")
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Getter
 @Setter
+@ToString(doNotUseGetters = true)
+@EqualsAndHashCode(doNotUseGetters = true, onlyExplicitlyIncluded = true)
 public class User implements Persistable<Long>, Serializable {
 
   private static final long serialVersionUID = -4564476945410560023L;
 
   @Id
   @GeneratedValue(strategy = IDENTITY)
+  @EqualsAndHashCode.Include
   private Long id;
 
   @Email
@@ -39,25 +48,34 @@ public class User implements Persistable<Long>, Serializable {
   @NotNull
   @NaturalId
   @Column(length = 128, unique = true, nullable = false)
+  @EqualsAndHashCode.Include
   private String email;
 
   @Size(max = 128)
-  @Column(length = 128)
+  @NotNull
+  @Column(length = 128, nullable = false)
   private String name;
 
   @Size(max = 128)
-  @Column(length = 128)
+  @NotNull
+  @Column(length = 128, nullable = false)
   private String surname;
 
   @Size(max = 128)
   @PhoneNumber
-  @Column(length = 128)
+  @NotNull
+  @Column(length = 128, nullable = false)
   private String phoneNumber;
 
   @Convert(converter = StatusConverter.class)
   @NotNull
   @Column(nullable = false)
+  @Builder.Default
   private Status status = OFFLINE;
+
+  @PastOrPresent
+  @Column(name = "last_online_change")
+  private Instant lastOnlineChange;
 
   @Override
   public Long getId() {
