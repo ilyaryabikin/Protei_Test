811d5d3d57daee8ebb030fed684804d5152f1c5d
Ilya Ryabikin <ilyaryabikin0708@gmail.com>
2021-02-22 03:09:59 +0300

    Add method to find users with specified status to UserRepository

--- src/main/java/ru/protei/test/repositories/UserRepository.java
+++ src/main/java/ru/protei/test/repositories/UserRepository.java
@@ -1,7 +1,14 @@
 package ru.protei.test.repositories;
 
+import java.util.List;
 import org.springframework.stereotype.Repository;
+import ru.protei.test.domains.Status;
 import ru.protei.test.domains.User;
 
 @Repository
-public interface UserRepository extends PersistableRepository<User, Long> {}
+public interface UserRepository extends PersistableRepository<User, Long> {
+
+  boolean existsByEmail(final String email);
+
+  List<User> findAllByStatus(final Status status);
+}
